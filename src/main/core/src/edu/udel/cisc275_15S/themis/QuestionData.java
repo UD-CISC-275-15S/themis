77834ebbbc04e18aa3d55a8ed5bd3b1c1418c37b
hauntze <chen.david9595@gmail.com>
2015-05-06 20:21:24 -0400

    Committing new QuestionData and testing if commit works

--- src/main/core/src/edu/udel/cisc275_15S/themis/QuestionData.java
+++ src/main/core/src/edu/udel/cisc275_15S/themis/QuestionData.java
@@ -2,8 +2,13 @@ package edu.udel.cisc275_15S.themis;
 
 import java.io.FileNotFoundException;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 //	This class is used for writing Data from to QuestionData.txt - will edit to be a csv soon
 //	QuestionData consists of:
@@ -13,46 +18,33 @@ import com.badlogic.gdx.files.FileHandle;
 //		if the question has been answered or not
 
 public class QuestionData {
-	public static FileHandle QuestionData = Gdx.files.local("Gamedata/QuestionData.csv");
+	public static File QuestionData = new File("Gamedata/QuestionData.txt");
 	
-	public QuestionData() {
-		QuestionData.writeString("Question,Number Of Times Wrong,Time Taken,Answered\"\n\"", false);
+	public static void createFile() throws FileNotFoundException {
+		PrintWriter writer = new PrintWriter(QuestionData);
+		writer.println("Question,Number Of Times Wrong,Time Taken,Completed\"\n\"");
 	}
 	
 	public static void processAnswer(String question, boolean correct, int time) throws FileNotFoundException {
-		String text = QuestionData.toString();
-		String newtext = "";
-		if(text.contains(question)){
-			int commaIndex = text.indexOf(",", text.indexOf(question));
-			int nextCommaIndex = text.indexOf(",", commaIndex + 1);
-			int thirdCommaIndex = text.indexOf(",", nextCommaIndex + 1);
-			int endLineIndex = text.indexOf("\"\n\"", thirdCommaIndex);
-			newtext.concat(text.substring(0, commaIndex + 1));
-			int numWrong = Integer.parseInt(text.substring(commaIndex + 1, nextCommaIndex));
-			int questionTime = Integer.parseInt(text.substring(nextCommaIndex + 1, thirdCommaIndex));
-			
-			if(!correct) {
-				newtext.concat(Integer.toString(numWrong++));
-				newtext.concat(",");
-				newtext.concat(Integer.toString(questionTime + time));
-				newtext.concat(text.substring(thirdCommaIndex, text.length() - 1));
-				QuestionData.writeString(newtext, false);
-			}
-			else {
-				newtext.concat(text.substring(nextCommaIndex, thirdCommaIndex + 1));
-				newtext.concat(Integer.toString(questionTime + time) + ",");
-				newtext.concat("true");
-				newtext.concat(text.substring(endLineIndex, text.length() - 1));
-				QuestionData.writeString(newtext, false);
-			}
-		}
+		Scanner infile = new Scanner(QuestionData);
+		PrintWriter writer = new PrintWriter(QuestionData);
 		
-		else {
-			if(!correct){
-				QuestionData.writeString(question + ",1," + time + ",false\"\n\"", true);
+		while(infile.hasNextLine()) {
+			String currentLine = infile.nextLine();
+			int slashIndex = currentLine.indexOf("/", 0);
+			int nextSlashIndex = currentLine.indexOf("/", slashIndex + 1);
+			int thirdSlashIndex = currentLine.indexOf("/", nextSlashIndex + 1);
+			int numWrong = Integer.parseInt(currentLine.substring(slashIndex + 1, nextSlashIndex));
+			int questionTime = Integer.parseInt(currentLine.substring(nextSlashIndex + 1, thirdSlashIndex));
+			
+			
+			if(currentLine.isEmpty()) {
+				if(correct) { writer.println(question+"/0/"+time+"/yes"); }
+				else { writer.println(question+"/1/"+time+"/no"); }
 			}
-			else {
-				QuestionData.writeString(question + ",0," + time + ",true\"\n\"", true);
+			else if(currentLine.startsWith(question)) {
+				if(correct) { writer.println(question+"/"+Integer.toString(numWrong)+"/"+Integer.toString(time+questionTime)+"/yes"); }
+				else { writer.println(question+"/"+Integer.toString(numWrong + 1)+"/"+Integer.toString(time+questionTime)+"/no"); }
 			}
 		}
 	}
